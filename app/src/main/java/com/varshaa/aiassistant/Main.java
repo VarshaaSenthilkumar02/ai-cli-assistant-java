bc5d5e5d2fdf3c95d79974f499fa2b6c300d91ba
VarshaaSenthilkumar <varshaasenthilkumar@VarshaaSenthilkumars-MacBook-Air.local>
2026-05-06 20:52:11 +0530

    Day 2: Received API response on asking question

--- app/src/main/java/com/varshaa/aiassistant/Main.java
+++ app/src/main/java/com/varshaa/aiassistant/Main.java
@@ -1,16 +1,43 @@
 package com.varshaa.aiassistant;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
+
+import static java.net.http.HttpRequest.BodyPublishers.ofString;
 
 public class Main {
 
-    public static void main(String[] args) {
+     static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ask something: ");
 
+        System.out.print("Ask something: ");
         String userInput = scanner.nextLine();
-        System.out.println("AI Assistant received: " + userInput);
+
+        // JSON request body for Ollama
+        String requestBody = """
+        {
+          "model": "llama3",
+          "prompt": "%s",
+          "stream": false
+        }
+        """.formatted(userInput);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:11434/api/generate"))
+                .header("Content-Type", "application/json")
+                .POST(ofString(requestBody))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("\nAI Response:\n" + response.body());
 
         scanner.close();
     }
