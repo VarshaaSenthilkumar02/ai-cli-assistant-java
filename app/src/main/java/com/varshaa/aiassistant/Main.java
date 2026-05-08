b10f08d58b5e6e9e0465f4ed479392d2440eb0aa
VarshaaSenthilkumar <varshaasenthilkumar@VarshaaSenthilkumars-MacBook-Air.local>
2026-05-08 22:37:07 +0530

    Day 4: Added logic to loop through AI chat

--- app/src/main/java/com/varshaa/aiassistant/Main.java
+++ app/src/main/java/com/varshaa/aiassistant/Main.java
@@ -17,39 +17,53 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ask something: ");
-        String userInput = scanner.nextLine();
-
-        // JSON request body for Ollama
-        String requestBody = """
-        {
-          "model": "llama3",
-          "prompt": "%s",
-          "stream": false
-        }
-        """.formatted(userInput);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:11434/api/generate"))
-                .header("Content-Type", "application/json")
-                .POST(ofString(requestBody))
-                .build();
-
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
-
+         HttpClient client = HttpClient.newHttpClient();
          ObjectMapper objectMapper = new ObjectMapper();
 
-         JsonNode jsonNode = objectMapper.readTree(response.body());
+         System.out.println("=== AI Assistant Started ===");
+         System.out.println("Type 'exit' to stop.\n");
 
-         // Step 6: Extract only "response" field
-         String aiResponse = jsonNode.get("response").asText();
+        while(true) {
+            System.out.print("You : ");
+            String userInput = scanner.nextLine();
 
-         // Step 7: Print clean output
-         System.out.println("\nAI:");
-         System.out.println(aiResponse);
+            if(userInput.equalsIgnoreCase("exit")) {
+                System.out.println("AI Assistance Stopped!");
+                break;
+            }
+
+            if(userInput.trim().isEmpty()) {
+                System.out.println("Please enter a valid input!!!");
+                continue;
+            }
+
+            // JSON request body for Ollama
+            String requestBody = """
+            {
+                "model": "llama3",
+                "prompt": "%s",
+                "stream": false
+            }""".formatted(userInput);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:11434/api/generate"))
+                    .header("Content-Type", "application/json")
+                    .POST(ofString(requestBody))
+                    .build();
+
+            // Step 5: Send request
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JsonNode jsonNode = objectMapper.readTree(response.body());
+
+            // Step 6: Extract only "response" field
+            String aiResponse = jsonNode.get("response").asText();
+
+            // Step 7: Print clean output
+            System.out.println("\nAI:");
+            System.out.println(aiResponse);
+        }
 
         scanner.close();
     }
