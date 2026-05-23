a46a9a8403741a5f90579f3db51e015ac0192a61
VarshaaSenthilkumar <varshaasenthilkumar@VarshaaSenthilkumars-MacBook-Air.local>
2026-05-23 19:02:11 +0530

    Day 18: Added registry pattern to handle the roles

--- app/src/main/java/com/varshaa/aiassistant/role/RoleFactory.java
+++ app/src/main/java/com/varshaa/aiassistant/role/RoleFactory.java
@@ -1,10 +1,19 @@
 package com.varshaa.aiassistant.role;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoleFactory {
+    public static final Map<String, Role> roleRegistry = new HashMap<>();
+
+    static {
+        roleRegistry.put("teacher", new TeacherRole());
+        roleRegistry.put("mentor", new MentorRole());
+        roleRegistry.put("interviewer", new InterviewerRole());
+        roleRegistry.put("default", new DefaultRole());
+    }
+
     public static Role getRole(String role) {
-        if(role.equalsIgnoreCase("teacher")) return new TeacherRole();
-        if(role.equalsIgnoreCase("interviewer")) return new InterviewerRole();
-        if(role.equalsIgnoreCase("mentor")) return new MentorRole();
-        return new DefaultRole();
+        return roleRegistry.getOrDefault(role.toLowerCase(), new DefaultRole());
     }
 }
